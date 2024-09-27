6d87854f165d005e48ca3cd5eaeb467ddcf5beb7
김예찬 <ycraah@gamil.com>
2024-09-27 22:38:09 +0900

    "중복 회원 가입 방지 테스트"

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -30,7 +31,20 @@ public class UserController {
       bindingResult.rejectValue("password2", "Passwords do not match", "2개의 비밀번호가 일치하지 않습니다.");
       return "signup_form";
     }
-    userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+
+    try{
+      userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+    } catch (DataIntegrityViolationException e){
+      e.printStackTrace();
+      bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+      return "signup_form";
+    } catch(Exception e){
+      e.printStackTrace();
+      bindingResult.reject("signupFailed", e.getMessage());
+      return "signup_form";
+    }
+
+
     return "redirect:/";
 
   }
