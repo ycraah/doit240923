736726c12ed0f1da06e5aa8f2d6861108a117faa
김예찬 <ycraah@gamil.com>
2024-10-19 20:00:15 +0900

    세팅

---
+++ src/main/java/com/ycraah/doit241019/Doit241019Application.java
@@ -0,0 +1,12 @@
+package com.ycraah.doit241019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doit241019Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Doit241019Application.class, args);
+  }
+}
---
+++ src/test/java/com/ycraah/doit241019/Doit241019ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycraah.doit241019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doit241019ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
